88d841319f35290409556a310637578429e4eb71
devanjana401 <anjanasasiaduvad@gmail.com>
2025-05-28 21:43:01 +0530

    Add new files

---
+++ 2D array with function.py
@@ -0,0 +1,40 @@
+def getArray():
+    a=int(input("Enter the row:"))
+    b=int(input("Enter the column:"))
+    array1 = []
+    array2 = []
+    result = []
+    print("Enter elements for Array1:")
+    for i in range(a):
+        row = []
+        for j in range(b):
+           element = int(input(f"[{i}][{j}]:"))
+           row.append(element)
+        array1.append(row)
+    print("Enter elements for Array2:")
+    for i in range(a):
+        row = []
+        for j in range(b):
+            element = int(input(f"[{i}][{j}]:"))
+            row.append(element)
+        array2.append(row)
+    for i in range(a):
+        row = []
+        for j in range(b):
+            row.append(array1[i][j] + array2[i][j])
+        result.append(row)
+    return array1, array2, result
+def displayArray(array1,array2,result):
+    print("Array1:")
+    for row in array1:
+        print(row)
+    print("Array2:")
+    for row in array2:
+        print(row)
+    print("Sum of Array1 and Array2:")
+    for row in result:
+        print(row)
+def main():
+    array1,array2,result = getArray()
+    displayArray(array1, array2,result)
+main()
