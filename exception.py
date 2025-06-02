b97c2d9833c217300c06371cd877278afa4d7d9b
devanjana401 <anjanasasiaduvad@gmail.com>
2025-06-02 14:13:01 +0530

    Add new files

--- exception.py
+++ exception.py
@@ -1,14 +1,8 @@
-class OnePressedError(Exception):
+class MyError(Exception):
     pass
-for i in range(3):
-    try:
-        print("\nMenu:")
-        print("1. Raise custom exception")
-        print("2. Just a normal option")
-        choice = input("Enter your choice: ")
-
-        if choice == "1":
-            raise OnePressedError("You pressed 1, which is not allowed!")
-        print("You selected option", choice)
-    except OnePressedError as e:
-        print("Custom Exception Caught:", e)
+choice = input("Press 1 to raise error: ")
+try:
+    if choice == "1":
+        raise MyError("Error: You pressed 1")
+except MyError as e:
+    print(e)
